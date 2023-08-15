449bc9bf106dedce99641d1d1f0ad5e06313858c
neropatti <28650266+neropatti@users.noreply.github.com>
2023-08-15 06:33:29 +0300

    starting to parse meta tags

--- dl.gd
+++ dl.gd
@@ -1,46 +1,20 @@
 extends VBoxContainer
 
-@onready var thumbnail_fetcher : HTTPRequest = $"thumbnail fetcher"
+var link : String
 
-const video_thumbnail_url : String = "https://img.youtube.com/vi/%s/hqdefault.jpg"
-var video_id : String = "gVC-nhVSH2c"
-
-func set_link(link : String) -> bool:
-	var video_id := get_video_id(link)
-	set_video_id(video_id)
+func set_link(new_link : String) -> bool:
+	link = new_link
+	%"webpage fetcher".request(new_link)
 	return true
-
-func get_video_id(link_or_id : String) -> String:
-	link_or_id = trim_prefixes(link_or_id, ["www.", "youtube.com/watch?v=", "youtu.be/"])
-	link_or_id = link_or_id.get_slice("&", 0)
-	assert(link_or_id.length() == 11) # Youtube video IDs are 11 characters long
-	return link_or_id
 
 func trim_prefixes(string : String, prefixes : Array [String]) -> String:
 	for prefix in prefixes:
 		string = string.trim_prefix(prefix)
 	return string
 
-## Set the video ID and refresh things that depend on it (currently just the thumbnail)
-func set_video_id(id : String):
-	video_id = id
-	refresh_thumbnail()
-
-func set_video_title(title : String):
+func set_title(title : String):
 	%title.text = title
 	%title.tooltip_text = title
-
-## Fetch a new thumbnail
-func refresh_thumbnail():
-	thumbnail_fetcher.request(video_thumbnail_url % video_id)
-	print("Request fired!")
-
-func _on_thumbnail_fetcher_request_completed(result, response_code, _headers, body : PackedByteArray):
-	if result != OK and response_code != 200:
-		print("Failed to fetch new thumbnail!")
-		return
-	var b := load_image_from_buffer(body)
-	%thumbnail.texture = ImageTexture.create_from_image(b)
 
 ## Load an image from a PackedByteArray. Tries to parse the data as a JPG, PNG, WEBP, TGA & BMP image, returning the first one that succeeds.
 func load_image_from_buffer(buffer : PackedByteArray) -> Image:
@@ -58,3 +32,44 @@ func load_image_from_buffer(buffer : PackedByteArray) -> Image:
 		return Image.load_from_file("res://icon.svg")
 	assert(not img.is_empty())
 	return img
+
+func _on_webpage_fetcher_request_completed(result, response_code, headers, body : PackedByteArray):
+	if result != OK or response_code != 200:
+		printerr("Failed to load page at link %s" % link)
+		return
+	
+	var page : String = body.get_string_from_utf8()
+	var info : Dictionary = {}
+	
+	var i : int = 0
+	while true:
+		i = page.find("<meta property", i)
+		print(i)
+		if i == -1:
+			break
+		var i2 = page.find(">", i)
+		if i2 == -1:
+			break
+		var meta_tag = page.substr(i, i2 - i + 1)
+		parse_meta_tag(meta_tag)
+		i += 1
+	
+	print("Finished!")
+	
+
+func parse_meta_tag(meta_tag : String) -> Array [String]:
+	assert(meta_tag.begins_with("<meta "))
+	assert(meta_tag.ends_with(">"))
+	assert(meta_tag.count("<") == 1 and meta_tag.count(">") == 1)
+	
+	var i := meta_tag.find("property=\"")
+	var i2 := meta_tag.find("\"", i + 10)
+	assert(i != -1 and i2 != -1)
+	var meta_property_name : String = meta_tag.substr(i + 10, i2 - i - 10)
+	
+	i = meta_tag.find("content=\"")
+	i2 = meta_tag.find("\"", i + 9)
+	assert(i != -1 and i2 != -1)
+	var meta_content : String = meta_tag.substr(i + 9, i2 - i - 9)
+	
+	return [meta_property_name, meta_content]
--- library.gd
+++ library.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var youtube_video_entry := preload("res://dl.tscn")
+var link_entry := preload("res://dl.tscn")
 
 # TODO: Figure out what domain the link is from
 # TODO: Do specific actions according to the domain
@@ -11,30 +11,14 @@ var youtube_video_entry := preload("res://dl.tscn")
 
 # I guess I can just have 2 entries on a dictionary that covers both XD
 
-var domains : Dictionary = {
-	"www.youtube.com" : youtube_video_entry,
-	"youtube.com" : youtube_video_entry,
-	"www.youtu.be" : youtube_video_entry, # (www.youtu.be is *not* a valid youtube link, including it here anyway)
-	"youtu.be" : youtube_video_entry,
-}
 
 func _on_link_input_text_submitted(link : String):
 #	print(link)
 #	print(get_youtube_video_id(link))
 	%"link input".text = ""
-#	var new_video_entry := video_entry.instantiate()
-#	%items.add_child(new_video_entry)
-#	new_video_entry.set_video_id(get_youtube_video_id(link))
-	
-	link = trim_prefixes(link, ["https://", "http://"])
-	if not domains.has(link):
-		# TODO: Come up with a "generic link entry" that's used for everything w/o special logic :)
-		# Maybe I actually just want to generically embed everything? Hmm
-		return
-	
-	var new_entry = domains[link].instantiate()
-	%items.add_child(new_entry)
-	new_entry.set_link(link)
+	var new_link_entry := link_entry.instantiate()
+	%items.add_child(new_link_entry)
+	new_link_entry.set_link(link)
 
 func trim_prefixes(string : String, prefixes : Array [String]) -> String:
 	for prefix in prefixes:
